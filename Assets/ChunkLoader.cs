fbfb75dc858a8b085e9df79f3710449dc5b3463d
Korpo-spec <casper22x@gmail.com>
2023-05-05 16:19:38 +0200

    ChunkLoader

--- Assets/ChunkLoader.cs
+++ Assets/ChunkLoader.cs
@@ -14,6 +14,8 @@ public class ChunkLoader : MonoBehaviour
 
     private Vector2Int _minValue;
     private Vector2Int _maxValue;
+
+    private Vector2Int _currentValue;
     // Start is called before the first frame update
     void Start()
     {
@@ -41,6 +43,13 @@ public class ChunkLoader : MonoBehaviour
         
         _maxValue.y = (int)(pos.y + viewDistance);
         _maxValue.y = _maxValue.y.RoundOff();
+
+
+        _currentValue.x = (int) (pos.x);
+        _currentValue.x = _currentValue.x.RoundOff();
+        
+        _currentValue.y = (int) (pos.y);
+        _currentValue.y = _currentValue.y.RoundOff();
         //Debug.Log(_maxValue);
     }
 
@@ -59,13 +68,43 @@ public class ChunkLoader : MonoBehaviour
         }
     }
 
+    private void LoadChunkRow(int rowNum)
+    {
+        
+    }
+
+    private void LoadChunkCol(int colNum)
+    {
+        
+    }
+
     
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 pos = transform.position;
+        if (pos.x < _currentValue.x -10)
+        {
+            LoadChunkCol(_minValue.x-10);
+        }
+
+        if (pos.x > _currentValue.x +10)
+        {
+            LoadChunkRow(_maxValue.x +10);
+        }
         
+        if (pos.y < _currentValue.y -10)
+        {
+            LoadChunkCol(_minValue.y-10);
+        }
+
+        if (pos.y > _currentValue.y +10)
+        {
+            LoadChunkRow(_maxValue.y);
+        }
     }
-    
-    
 }
+    
+    
+
